c44cc04f8b07aba4255b0c712d2440d2d881185b
Wilian Pereira <wilian_pereira92@outlook.com>
2024-03-12 19:24:54 -0300

    Test: Post

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -8,18 +8,39 @@ describe("Teste requisição", () => {
     });
   });
 
+  it("Post", () => {
+    const authorization = `Bearer 640112a389638f4ef5decd694fb59e4a0e0e1c3f7f2bdf91129976090fcf566f`;
+    const options = {
+      method: "POST",
+      url: "https://gorest.co.in/public/v2/users",
+      headers: {
+        authorization,
+      },
+      body: {
+        name: "teste 4",
+        gender: "male",
+        email: "tenali.test4@15ce.com",
+        status: "active",
+      },
+    };
+
+    cy.request(options).then((response) => {
+      expect(response.status).to.equal(201);
+    });
+  });
+
   it("Delete", () => {
-    const authorization = `bearer 640112a389638f4ef5decd694fb59e4a0e0e1c3f7f2bdf91129976090fcf566f`;
+    const authorization = `Bearer 640112a389638f4ef5decd694fb59e4a0e0e1c3f7f2bdf91129976090fcf566f`;
     const options = {
       method: "DELETE",
-      url: "https://gorest.co.in/public/v2/users/2139214",
+      url: "https://gorest.co.in/public/v2/users/6775106",
       headers: {
         authorization,
       },
     };
 
     cy.request(options).then((response) => {
-      expect(response.status).to.equal(200);
+      expect(response.status).to.equal(204);
     });
   });
 });
